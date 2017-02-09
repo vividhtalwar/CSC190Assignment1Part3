970e43e701b7060b07959654b85cfe0527d341c2
vividhtalwar <vividhtalwar@gmail.com>
2017-02-08 19:00:29 -0500

    Saved The World!!!

---
+++ Warhead.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cybermissile;
+
+/**
+ *
+ * @author csc190
+ */
+public class Warhead {
+   /* public void explode(){
+        System.out.println("Planting the mushroom now ...");
+    }
+    */
+
+    public void selfDestruct(){
+        System.out.println("Self destruct!");
+    }
+}
